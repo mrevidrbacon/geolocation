307a4834d2c68d80d09e0bbf59e86cf78a8f7d27
TheMagnificientBoneless <ayvarlopez@gmail.com>
2024-03-04 07:42:02 -0600

    message

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Enable CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+// Endpoint to receive and store data
+// Endpoint to receive and store data
+app.post('/storeData', (req, res) => {
+    const { latitude, longitude } = req.body;
+    const headers = req.headers;
+    console.log('All headers:', headers);
+
+    // Save headers data to a file
+    fs.writeFile('./data/headers.json', JSON.stringify(headers), (err) => {
+        if (err) throw err;
+        console.log('Headers data saved successfully.');
+    });
+
+    // Save location data to a file
+    const locationData = { latitude, longitude };
+    fs.writeFile('./data/location.json', JSON.stringify(locationData), (err) => {
+        if (err) throw err;
+        console.log('Location data saved successfully.');
+    });
+
+    console.log('Received location:', latitude);
+    console.log('Received longitude:', longitude);
+    res.send('ok');
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
