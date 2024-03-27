2b3c876e3725d28bfa8d7ab172e5ec82c40e8f19
TheMagnificientBoneless <ayvarlopez@gmail.com>
2024-03-26 18:39:33 -0600

    store filename date and time

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require('cors'); // Importa el módulo cors
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import cors from "cors";
+import { fileURLToPath } from "url"; // Import fileURLToPath function from url module
+import { dirname } from "path"; // Import dirname function from path module
+
+const __filename = fileURLToPath(import.meta.url); // Convert the import meta URL to a file path
+const __dirname = dirname(__filename); // Get the directory name
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(cors());
 
 // Enable CORS
@@ -17,29 +22,31 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
-
+// Function to pad single-digit numbers with leading zeros
+function padWithZero(number) {
+    return (number < 10 ? '0' : '') + number;
+}
 // Endpoint to receive and store data
-// Endpoint to receive and store data
-app.post('/storeData', (req, res) => {
+app.post('/storeData', async (req, res) => {
     const { latitude, longitude } = req.body;
     const headers = req.headers;
-    const currentDate = new Date().toISOString().split('T')[0];
-    const currentTime = new Date().toISOString().split('T')[1].substring(0, 5); // Extracting hours and minutes
-    const currentDateTime = `${currentDate}-${currentTime}`;
+    const currentDate = new Date();
+    const currentDateFormatted = `${currentDate.getFullYear()}-${padWithZero(currentDate.getMonth() + 1)}-${padWithZero(currentDate.getDate())}`;
+    const currentTimeFormatted = `${padWithZero(currentDate.getHours())}-${padWithZero(currentDate.getMinutes())}-${padWithZero(currentDate.getSeconds())}`;
+    const currentDateTime = `${currentDateFormatted}-${currentTimeFormatted}`;
 
     // Save headers data to a file
-    fs.writeFile(`./data/headers${currentDateTime}.json`, JSON.stringify({ headers, currentDateTime }), (err) => {
+    fs.writeFile(`${__dirname}/data/headers-${currentDateTime}.json`, JSON.stringify({ headers, currentDateTime }), (err) => {
         if (err) throw err;
         console.log('Headers data saved successfully.');
     });
 
     // Save location data to a file
-    const locationData = { latitude, longitude, currentDateTime };
-    fs.writeFile(`./data/location${currentDateTime}.json`, JSON.stringify(locationData), (err) => {
+    fs.writeFile(`${__dirname}/data/location-${currentDateTime}.json`, JSON.stringify({ latitude, longitude, currentDateTime }), (err) => {
         if (err) throw err;
         console.log('Location data saved successfully.');
     });
@@ -48,7 +55,6 @@ app.post('/storeData', (req, res) => {
     console.log('Received longitude:', longitude);
     res.send('ok');
 });
-
 
 // Start the server
 app.listen(PORT, () => {
